21a21a8b49c580cd78a0c4e7464c007b2efa8ff9
ZoveRKI <shiyu5645@gmail.com>
2024-09-06 22:56:10 +0900

    git init

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+phpinfo();
+echo 'Start-Process -FilePath .\php-cgi.exe -ArgumentList "-b 127.0.0.1:9000" -WindowStyle Hidden<br>';
+echo 'Start-Process -FilePath D:\Language\php\Version\php-8.3.7-nts-Win32-vs16-x64\php-cgi.exe -ArgumentList "-b 127.0.0.1:9000" -WindowStyle Hidden<br>';
+echo '123';
