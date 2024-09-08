39801924f8c381fbbd79ca4c549a109ef14f7033
ZoveRKI <shiyu5645@gmail.com>
2024-09-08 19:57:39 +0900

    version1

--- learn/php-20240907.php
+++ learn/php-20240907.php
@@ -1,2 +1,22 @@
+<?php
+$a_bool = true;   // a bool
+$a_bool2 = false;
+$a_str  = "foo";  // a string
+$a_str2 = 'foo2';  // a string
+$an_int = 12;     // an int
+echo get_debug_type($a_bool), "\n";
+echo get_debug_type($a_str), "\n";
+
+if (is_int($an_int)) {
+    $an_int += 4;
+}
+var_dump($an_int);
+
+if (is_string($a_str)) {
+    echo "String: $a_bool"."\n";
+    echo "String: $a_bool2"."\n";
+    echo "String: $an_int";
+}
+
 
 
